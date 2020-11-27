91ffee630a2a2faae24a4b37763bc18616b7fc2f
Andrew Dzobko <mytyber@gmail.com>
2020-11-26 20:49:35 -0800

    Modified jogin.js to a different submit path

--- static/login-out.js
+++ static/login-out.js
@@ -2,7 +2,7 @@
 function onSignIn(_googleUser){
     var id_token = googleUser.getAuthResponse().id_token;
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://ridna-library.herokuapp.com/login'); // Change the url after we move to our subdomain
+    xhr.open('POST', '/login'); // Change the url after we move to our subdomain
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
         console.log('Signed in as: ' + xhr.responseText);
