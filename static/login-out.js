05c580372c90e108767f601dfbab2e28d51d18c5
Andrew Dzobko <mytyber@gmail.com>
2021-02-13 20:49:44 -0800

    Added session monitoring

--- static/login-out.js
+++ static/login-out.js
@@ -1,8 +1,9 @@
+var auth2;
 // Sign in function to pass the user info to session
 function onSignIn(googleUser){
     var id_token = googleUser.getAuthResponse().id_token;
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/login'); // Change the url after we move to our subdomain
+    xhr.open('POST', '/login');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
         console.log('Signed in as: ' + googleUser.getBasicProfile().getName());
@@ -11,6 +12,38 @@ function onSignIn(googleUser){
     xhr.send('idtoken=' + id_token);
     setTimeout(function(){window.location.href = "/";}, 1000);
 }
+
+/**
+ * Initializes the Sign-In client.
+ */
+var initClient = function() {
+    gapi.load('auth2', function(){
+        /**
+         * Retrieve the singleton for the GoogleAuth library and set up the
+         * client.
+         */
+        auth2 = gapi.auth2.init({
+            client_id: '381057537277-pmtvd839t2a60eslm0bvke78rviilr5l.apps.googleusercontent.com'
+        });
+
+        // Attach the click handler to the sign-in button
+        auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
+    });
+};
+
+/**
+ * Handle successful sign-ins.
+ */
+var onSuccess = function() {
+    setTimeout(function(){window.location.href = "/";}, 1000);
+ };
+
+/**
+ * Handle sign-in failures.
+ */
+var onFailure = function(error) {
+    console.log(error);
+};
 
 // Logout function
 function signOut() {
