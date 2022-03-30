6b481e01d7cfc7b28401a2c0ee63019ce8f1ab80
Andrew Dzobko <mytyber@gmail.com>
2022-03-30 15:30:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -542,7 +542,7 @@ def login():
         from pprint import pprint
         print("=======================================")
         print("Got request")
-        pprint(dir(request))
+        pprint(vars(request))
         print("=======================================")
         token = request.form["idtoken"]
         try:
