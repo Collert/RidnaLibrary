80551fcb32e8a3de4834294d00e3467931b61d60
Andrew Dzobko <mytyber@gmail.com>
2022-03-30 18:33:26 -0700

    Update login-out.js

--- static/login-out.js
+++ static/login-out.js
@@ -9,11 +9,11 @@ function onSignIn(response){
         console.log('Signed in as: ' + responsePayload.name);
     };
     console.log(responsePayload);
-    xhr.send('id=' + responsePayload.sub);
-    xhr.send('email=' + responsePayload.email);
-    xhr.send('first=' + responsePayload.given_name);
-    xhr.send('last=' + responsePayload.family_name);
-    xhr.send('pic=' + responsePayload.picture);
+    xhr.send('id=' + responsePayload.sub + '&email=' + responsePayload.email + '&first=' + responsePayload.given_name + '&last=' + responsePayload.family_name + '&pic=' + responsePayload.picture);
+    //xhr.send('email=' + responsePayload.email);
+    //xhr.send('first=' + responsePayload.given_name);
+    //xhr.send('last=' + responsePayload.family_name);
+    //xhr.send('pic=' + responsePayload.picture);
     setTimeout(() => {
         window.location.replace("/");
     }, 2000);
