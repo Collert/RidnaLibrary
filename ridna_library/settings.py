fcdde05f486cb8244b35685a34f235309925edff
Andrew Dzobko <mytyber@gmail.com>
2026-05-29 12:47:57 -0700

    feat: Update database settings for development and add migration for loan date fields

--- ridna_library/settings.py
+++ ridna_library/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'ridna_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if DEBUG and False:
+if DEBUG:
     # Development: Use SQLite
     DATABASES = {
         'default': {
